92dbd2487fa34bc3e3713acec1bf788a4d64f514
MOdest Wilton <tatohmodest525@gmail.com>
2023-12-04 17:09:53 +0100

    this is just what happens

--- calculator.js
+++ calculator.js
@@ -1,26 +1,84 @@
-// Function to append the clicked button value to the display
-function appendToDisplay(value) {
+document.addEventListener('DOMContentLoaded', function() {
   const display = document.getElementById('display');
-  display.value += value;
-}
 
-// Function to clear the display
-function clearDisplay() {
-  const display = document.getElementById('display');
-  display.value = '';
-}
-
-// Function to calculate the expression entered in the display
-function calculate() {
-  const display = document.getElementById('display');
-  const expression = display.value;
-
-  // Using the eval() function to evaluate the expression
-  // Note: The eval() function is used here for simplicity, but it's generally not recommended for security reasons in production code.
-  try {
-    const result = eval(expression);
-    display.value = result;
-  } catch (error) {
-    display.value = 'Error';
+  function appendToDisplay(value) {
+    display.value += value;
   }
-}
+
+  function clearDisplay() {
+    display.value = '';
+  }
+
+  function calculate() {
+    const expression = display.value;
+
+    try {
+      const result = eval(expression);
+      display.value = result;
+    } catch (error) {
+      display.value = 'Error';
+    }
+  }
+  document.getElementById('btn-0').addEventListener('click', function(){
+    appendToDisplay('0')
+})
+
+  document.getElementById('btn-1').addEventListener('click', function() {
+    appendToDisplay('1');
+  });
+  document.getElementById('btn-2').addEventListener('click', function() {
+    appendToDisplay('2');
+  });
+  document.getElementById('btn-3').addEventListener('click', function() {
+    appendToDisplay('3');
+  });
+  document.getElementById('btn-4').addEventListener('click', function() {
+    appendToDisplay('4');
+  });
+  document.getElementById('btn-5').addEventListener('click', function() {
+    appendToDisplay('5');
+  });
+  document.getElementById('btn-6').addEventListener('click', function() {
+    appendToDisplay('6');
+  });
+  document.getElementById('btn-7').addEventListener('click', function() {
+    appendToDisplay('7');
+  });
+  document.getElementById('btn-8').addEventListener('click', function() {
+    appendToDisplay('8');
+  });
+  document.getElementById('btn-9').addEventListener('click', function() {
+    appendToDisplay('9');
+  });
+
+
+
+
+  document.getElementById('btn-add').addEventListener('click', function() {
+    appendToDisplay('+');
+  });
+
+  document.getElementById('btn-subtract').addEventListener('click', function() {
+    appendToDisplay('-');
+  });
+
+  document.getElementById('btn-multiply').addEventListener('click', function() {
+    appendToDisplay('*');
+  });
+
+  document.getElementById('btn-divide').addEventListener('click', function() {
+    appendToDisplay('/');
+  });
+
+  // Add event listeners for other operator buttons in a similar way
+
+  // Event listener for equal button
+  document.getElementById('btn-equal').addEventListener('click', function() {
+    calculate();
+  });
+
+  // Event listener for clear button
+  document.getElementById('btn-clear').addEventListener('click', function() {
+    clearDisplay();
+  });
+});
